bf2cde2f7bce8d18af32160c749cbbf7a5ca7b38
amy <22689376@student.uwa.edu.au>
2022-09-07 16:37:59 +1000

    echidna example code

--- .flaskenv
+++
@@ -1,1 +0,0 @@
-FLASK_APP=curriculum_mapper.pypip install flask-migrate
--- app/static/main.js
+++
@@ -1,8 +0,0 @@
-/* The burger button that transformed into a cross */
-    function burgerFunction(event) {
-    if (event.classList.contains("change")) {
-        event.classList.remove("change");
-    } else {
-        event.classList.add("change");
-    }
-    }
