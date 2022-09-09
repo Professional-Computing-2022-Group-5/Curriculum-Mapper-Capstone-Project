8774ce9301988fbac9abd3eab9bcc8beaec91587
Yupeng Li <lypa520@gmail.com>
2022-09-09 10:16:28 +0800

    use jolt API to query the database

--- app/neo4jDB.py
+++ app/neo4jDB.py
@@ -1,5 +1,6 @@
 from py2neo import Graph,NodeMatcher,cypher,Node,Relationship
 import json
+from app import api
 
 # graphDB -> Connect the neo4j database
 # Graph("neo4j is running on localhost and port 7474", auth=("neo4j_username", "neo4j_password"))
@@ -7,21 +8,9 @@ graphDB = Graph("http://localhost:7474", auth=("neo4j","test"))
 
 def search_by_query(query):
     try:
-        # query -> neo4j cypher query from the user input
-        # run(): execute the query and return the result
-        # data(): format the result (a list of dict)
-        queryResult = graphDB.run(query).data()  #[dict,dict,dict, ...]
-        list_data = []  #[Path/Node, Path/Node, Path/Node, ...]
-
-        # "i" is a dict type
-        for i in queryResult: 
-            list_data.extend(i.values())
-
-        # "item" is a Path/Node object
-        for i, item in enumerate(list_data):
-            #get Node object from Path object
-            list_data[i] = list_data[i].nodes 
-        return list_data 
+        #using jolt API to get the data to the frontend
+        data = api.joltAPI(query) 
+        return data
     except:
         # return error message if the query is invalid
         return {'status': 'error'}
