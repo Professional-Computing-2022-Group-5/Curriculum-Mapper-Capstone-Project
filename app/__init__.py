f1a74953f1ce060554c97fd56767911fe0042ccc
Yupeng Li <lypa520@gmail.com>
2022-09-28 11:06:54 +0800

    enable CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 # create the application object
 app = Flask(__name__)
+# enable CORS
+CORS(app,supports_credentials=True)
 
 from app import routes, neo4jDB, api
