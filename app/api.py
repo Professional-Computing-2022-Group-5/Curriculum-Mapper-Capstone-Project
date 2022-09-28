a5f6b5fb744cbf35847c43f5d249ddcc283fea12
Yupeng Li <lypa520@gmail.com>
2022-09-28 11:13:09 +0800

    added displayName and color attribute to the return data

--- app/api.py
+++ app/api.py
@@ -62,17 +62,65 @@ def joltAPI(user_query):
                             links.append(link)
                         link['property'] = rel_property
 
+    # add displayName and color attribute to the return data
+    for item in nodes:
+        node_data = graphDB.nodes.get(item['id'])
+        labels = set(node_data.labels)
+        color = {
+            'CBoK': 'orange',
+            'Outcome': 'green',
+            'Unit': 'brown',
+            'AQFcategory': 'blue',
+            'AQFoutcome': 'pink',
+            'Sub': 'purple',
+            'Top': 'red'
+        }
+        item['labels'] = list(labels)
+        if 'CBoK' in list(labels):
+            item['displayName'] = item['area']
+        elif 'Outcome' in list(labels):
+            item['displayName']= item['level']
+        elif 'Unit' in list(labels):
+            item['displayName']= item['title']
+        elif 'Course' in list(labels):
+            item['displayName'] = item['title']
+        elif 'AQFcategory' in list(labels):
+            item['displayName'] = item['description']
+        elif 'AQFoutcome' in list(labels):
+            item['displayName'] = item['description']
+        elif 'Activity' in list(labels):
+            item['displayName'] = item['activity']
+        elif item['name'] != None:
+            item['displayName'] = item['name']
+        else:
+            item['displayName'] = list(labels)[0]
+
+        if list(labels)[0] in color.keys():
+            if 'Sub' in list(labels):
+                item['color'] = color['Sub']
+            elif 'Top' in list(labels):
+                item['color'] = color['Top']
+            else:
+                item['color'] = color[list(labels)[0]]
+        else:
+            item['color']  = 'grey'
+
+    for item in links:
+        link_data = graphDB.relationships.get(item['property']['id'])
+        item['labels'] = type(link_data).__name__
+        item['displayName'] = type(link_data).__name__
+
     # remove duplicate nodes
     id_list = []
     node_list = []
     for node in nodes:
         if node['id'] not in id_list:
-            # print(node['id'])
             node_list.append(node)
             id_list.append(node['id'])
 
     return_data['nodes'] = node_list
     return_data['links'] = links
+    
     # empty data handling
     if len(nodes) == 0 and len(links) == 0:
         return {'status':'empty_data'}
