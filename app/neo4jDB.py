139ddba65bb52bdaf9637dc16340618e4535925c
Yupeng Li <lypa520@gmail.com>
2022-09-29 19:42:43 +0800

    added return error message

--- app/neo4jDB.py
+++ app/neo4jDB.py
@@ -20,53 +20,57 @@ def search_by_query(query):
         return {'status': 'joltAPI_error'}
 
 def create_by_user(data):
-    for item in data:
-        #create a empty node object（source_node）
-        source_node = Node()
-        #get the data from the frontend (label,name)
-        for (key,value) in zip(item['source'].keys(),item['source'].values()):
-            if key == 'label':
-                #assign a name to the previously created node object
-                source_node.add_label(value)
+    try:
+        for item in data:
+            #create a empty node object（source_node）
+            source_node = Node()
+            #get the data from the frontend (label,name)
+            for (key,value) in zip(item['source'].keys(),item['source'].values()):
+                if key == 'label':
+                    #assign a name to the previously created node object
+                    source_node.add_label(value)
+                else:
+                    #add properties other than label(name, age, height, weight ...)
+                    source_node[key] = value
+            #query whether the current node already exists in the database (check both label and name)
+            source_match = NodeMatcher(graphDB).match(item['source']['label'],name=item['source']['name'])
+            if source_match.__len__() == 0:
+                #not exist: add the currently created node(source_node) to the database
+                graphDB.create(source_node)
             else:
-                #add properties other than label(name, age, height, weight ...)
-                source_node[key] = value
-        #query whether the current node already exists in the database (check both label and name)
-        source_match = NodeMatcher(graphDB).match(item['source']['label'],name=item['source']['name'])
-        if source_match.__len__() == 0:
-            #not exist: add the currently created node(source_node) to the database
-            graphDB.create(source_node)
-        else:
-            #exist: directly copy the queried node to source_node
-            source_node = source_match.first()
+                #exist: directly copy the queried node to source_node
+                source_node = source_match.first()
 
-        #same as above, just processing the target node
-        target_node = Node()
-        for (key,value) in zip(item['target'].keys(),item['target'].values()):
-            if key == 'label':
-                target_node.add_label(value)
+            #same as above, just processing the target node
+            target_node = Node()
+            for (key,value) in zip(item['target'].keys(),item['target'].values()):
+                if key == 'label':
+                    target_node.add_label(value)
+                else:
+                    target_node[key] = value
+            target_match = NodeMatcher(graphDB).match(item['target']['label'], name=item['target']['name'])
+            if target_match.__len__() == 0:
+                graphDB.create(target_node)
             else:
-                target_node[key] = value
-        target_match = NodeMatcher(graphDB).match(item['target']['label'], name=item['target']['name'])
-        if target_match.__len__() == 0:
-            graphDB.create(target_node)
-        else:
-            target_node = target_match.first()
+                target_node = target_match.first()
 
-        #handling the relationship
-        rel_match = RelationshipMatcher(graphDB) #create a query object
-        for (key,value) in zip(item['relationship'].keys(),item['relationship'].values()):
-            if key == 'label':
-                #relationship object :（‘startNode’, 'knows', 'endNode'）
-                rel = Relationship(source_node,value,target_node)
-                #any properties except label are added to the relationship object
-                for (key1, value1) in zip(item['relationship'].keys(), item['relationship'].values()):
-                    if key1 != 'label':
-                        rel[key1] = value1
-                #Use the previously created query object
-                #query whether the current relationship already exists in the database(relationship between source_node and target_node)
-                if len(rel_match.match([source_node,target_node],r_type=value)) == 0:
-                    graphDB.create(rel)
+            #handling the relationship
+            rel_match = RelationshipMatcher(graphDB) #create a query object
+            for (key,value) in zip(item['relationship'].keys(),item['relationship'].values()):
+                if key == 'label':
+                    #relationship object :（‘startNode’, 'knows', 'endNode'）
+                    rel = Relationship(source_node,value,target_node)
+                    #any properties except label are added to the relationship object
+                    for (key1, value1) in zip(item['relationship'].keys(), item['relationship'].values()):
+                        if key1 != 'label':
+                            rel[key1] = value1
+                    #Use the previously created query object
+                    #query whether the current relationship already exists in the database(relationship between source_node and target_node)
+                    if len(rel_match.match([source_node,target_node],r_type=value)) == 0:
+                        graphDB.create(rel)
+        return {'status':'create_success'}
+    except:
+        return {'status':'create_failed'}
 
 # delete nodes and relationships by user
 def deleteEntity(id, item):
