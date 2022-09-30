5584e06213bf8e9e327da1cd26dd3be8ac3638bb
Yupeng Li <lypa520@gmail.com>
2022-09-30 14:19:23 +0800

    upload front end code

---
+++ client/src/index.js
@@ -0,0 +1,74 @@
+import React from 'react';
+//import React, { useRef, useEffect } from 'react';
+
+import { createRoot } from "react-dom/client";
+import './style.css';
+//import App from './App';
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import {
+  Home,
+  Register,
+  Login,
+  Query,
+  ReportGen,
+  Navigation,
+  Footer
+} from "./templates";
+
+/*var data = {
+  nodes: [{ id: "A"}, { id: "B" }, { id: "C" }, { id: "D" }],
+  links: [
+    { source: "B", target: "C", value: 8 },
+    { source: "C", target: "D", value: 10 },
+    { source: "D", target: "A", value: 6 },
+    { source: "B", target: "A", value: 6 },
+    { source: "B", target: "D", value: 6 },
+    { source: "D", target: "D", value: 6, curvature: 0.3 }
+  ]
+};*/
+/*
+var data = {
+  nodes:[
+    {"availabilities":"Semester 2 2021, Crawley (Face to face); Semester 2 2021, Crawley (Online-TT) [Contact hours: n/a];","credit":6, "deleted":false, "id":183,"programmingBased":true,"title":"Computational Data Analysis","type":"node","unitCode":"CITS4009"},
+    {"availabilities":"Semester 2 2021, Crawley (Face to face); Semester 2 2021, Crawley (Online-TT) [Contact hours: n/a];","credit":6,"deleted":false,"id":183,"programmingBased":true,"title":"Computational Data Analysis","type":"node","unitCode":"CITS4009"},
+    {"deleted":false,"describe":"present and defend opinions by making judgments about information. validity of ideas, or quality of work based on a set of criteria.","id":292,"level":"IV.Analyzing","outcome":"(5) communicate effectively with stakeholders.","outcomeId":"5","type":"node","unitCode":"CITS4009"},
+    {"availabilities":"Semester 2 2021, Crawley (Face to face); Semester 2 2021, Crawley (Online-TT) [Contact hours: n/a];","credit":6,"deleted":false,"id":183,"programmingBased":true,"title":"Computational Data Analysis","type":"node","unitCode":"CITS4009"},
+    {"availabilities":"Semester 2 2021, Crawley (Face to face); Semester 2 2021, Crawley (Online-TT) [Contact hours: n/a];","credit":6,"deleted":false,"id":183,"programmingBased":true,"title":"Computational Data Analysis","type":"node","unitCode":"CITS4009"},
+    {"deleted":false,"describe":"Exhibit memory of previously learned material by recalling facts, terms,basic concepts, and answers.","id":289,"level":"I.Remembering","outcome":"(2) select appropriate data visualisation options; ","outcomeId":"2","type":"node","unitCode":"CITS4009"},
+    {"availabilities":"Semester 2 2021, Crawley (Face to face); Semester 2 2021, Crawley (Online-TT) [Contact hours: n/a];","credit":6,"deleted":false,"id":183,"programmingBased":true,"title":"Computational Data Analysis","type":"node","unitCode":"CITS4009"},
+    {"availabilities":"Semester 2 2021, Crawley (Face to face); Semester 2 2021, Crawley (Online-TT) [Contact hours: n/a];","credit":6,"deleted":false,"id":183,"programmingBased":true,"title":"Computational Data Analysis","type":"node","unitCode":"CITS4009"},
+    {"deleted":false,"describe":"Present and defend opinions by making judgments about information, validity of ideas, or quality of work based on a set of criteria.","id":291,"level":"V. Evaluating","outcome":"(4) critically assess the outcomes of a data analysis; ","outcomeId":"4","type":"node","unitCode":"CITS4009"}],
+  links:[
+      {"property":{"deleted":false,"id":241,"type":"relationship"},"source":183,"target":292},
+      {"property":{"deleted":false,"id":238,"type":"relationship"},"source":183,"target":289},
+      {"property":{"deleted":false,"id":240,"type":"relationship"},"source":183,"target":291}],
+}
+*/
+
+
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(  
+  <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      <Route path="/query" element={<Query />} />
+      <Route path="/reportGen" element={<ReportGen />} />
+    </Routes>
+
+    <Footer />
+  </Router>
+  );
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
---
+++ client/src/templates/query.jsx
@@ -0,0 +1,112 @@
+import React, {useState } from "react";
+import httpClient from "./httpClient.js";
+import GraphComponent from "./graphComponent";
+import TableComponent from "./tableComponent";
+/*
+var resultData =  ({"links":[
+    {"property":{"deleted":false,"id":241,"type":"relationship"},"source":183,"target":292},
+    {"property":{"deleted":false,"id":238,"type":"relationship"},"source":183,"target":289},
+    {"property":{"deleted":false,"id":240,"type":"relationship"},"source":183,"target":291}],
+"nodes":
+    [{"availabilities":"Semester 2 2021, Crawley (Face to face); Semester 2 2021, Crawley (Online-TT) [Contact hours: n/a];","credit":6,"deleted":false,"id":183,"programmingBased":true,"title":"Computational Data Analysis","type":"node","unitCode":"CITS4009"},
+    {"availabilities":"Semester 2 2021, Crawley (Face to face); Semester 2 2021, Crawley (Online-TT) [Contact hours: n/a];","credit":6,"deleted":false,"id":183,"programmingBased":true,"title":"Computational Data Analysis","type":"node","unitCode":"CITS4009"},
+    {"deleted":false,"describe":"present and defend opinions by making judgments about information. validity of ideas, or quality of work based on a set of criteria.","id":292,"level":"IV.Analyzing","outcome":"(5) communicate effectively with stakeholders.","outcomeId":"5","type":"node","unitCode":"CITS4009"},
+    {"availabilities":"Semester 2 2021, Crawley (Face to face); Semester 2 2021, Crawley (Online-TT) [Contact hours: n/a];","credit":6,"deleted":false,"id":183,"programmingBased":true,"title":"Computational Data Analysis","type":"node","unitCode":"CITS4009"},
+    {"availabilities":"Semester 2 2021, Crawley (Face to face); Semester 2 2021, Crawley (Online-TT) [Contact hours: n/a];","credit":6,"deleted":false,"id":183,"programmingBased":true,"title":"Computational Data Analysis","type":"node","unitCode":"CITS4009"},
+    {"deleted":false,"describe":"Exhibit memory of previously learned material by recalling facts, terms,basic concepts, and answers.","id":289,"level":"I.Remembering","outcome":"(2) select appropriate data visualisation options; ","outcomeId":"2","type":"node","unitCode":"CITS4009"},
+    {"availabilities":"Semester 2 2021, Crawley (Face to face); Semester 2 2021, Crawley (Online-TT) [Contact hours: n/a];","credit":6,"deleted":false,"id":183,"programmingBased":true,"title":"Computational Data Analysis","type":"node","unitCode":"CITS4009"},
+    {"availabilities":"Semester 2 2021, Crawley (Face to face); Semester 2 2021, Crawley (Online-TT) [Contact hours: n/a];","credit":6,"deleted":false,"id":183,"programmingBased":true,"title":"Computational Data Analysis","type":"node","unitCode":"CITS4009"},
+    {"deleted":false,"describe":"Present and defend opinions by making judgments about information, validity of ideas, or quality of work based on a set of criteria.","id":291,"level":"V. Evaluating","outcome":"(4) critically assess the outcomes of a data analysis; ","outcomeId":"4","type":"node","unitCode":"CITS4009"}]
+})
+*/
+var response = {};
+
+const Query = () => {
+    //-----------------------------------------------------------------------------------GLOBAL VARIABLES
+    const [query, setQuery] = useState("");
+    const [graphActive, setGraphActive] = useState("noGraph");
+    
+    //-----------------------------------------------------------------------------------FUNCTIONS
+    // EXECUTE & QUERY AND GET THE RETURN DATA
+    const executeQuery = async (query) => {
+        console.log(query);
+        try {
+            const dbData = await httpClient.post("//localhost:5000/query", {
+                query
+            });
+
+            console.log("DBDATA _ QUERY.js")
+            console.log(dbData.data)
+            if (dbData.data !== undefined){
+                console.log("IN FINALLY")
+
+                console.log("FROM DOOO GRAPH")
+                response = JSON.parse(JSON.stringify(dbData.data))
+                Object.freeze(response);
+                console.log(response)
+                setGraphActive("haveData")
+                //const forceRef = useRef(null);
+                //console.log(inputs)
+        
+                //useEffect(() => {
+                //    forceRef.current.d3Force("charge").strength(-400);
+                //});
+
+            }
+
+            //setResponse(JSON.parse(JSON.stringify(dbData.data)))
+            //response = JSON.parse(JSON.stringify(dbData.data))
+            //setResponse(dbData.data)
+            //console.log(response)
+
+
+        } catch (error) {
+            if (error.response.status === 401) {
+                alert("Invalid Query");
+            }
+        } //finally {
+        //console.log("IN FINALLY")
+        //console.log(response)
+        //setResponse(response)
+        //console.log(resp)
+        //setGraphActive("showGraph");
+    //}
+    };
+
+    if (graphActive === "haveData"){
+        console.log(response)
+        setGraphActive("showGraph");
+    }
+
+    //-----------------------------------------------------------------------------------RETURN
+    return ( 
+        <div className="container-fluid">
+
+            <div className = "container-fluid">
+
+            <div className="container-fluid row">
+                <h1 className="font-weight-light">Query Page</h1>
+                
+                {graphActive !== "showGraph" && <label>Input Query: </label>}
+                {graphActive !== "showGraph" && <input type = "text" value={query} onChange={(e)=> setQuery(e.target.value)}/>}
+                {graphActive !== "showGraph" && <button type="button" onClick={() => executeQuery(query)}>Submit</button>}
+                </div>
+            </div>
+
+            <div className="container-fluid row xs=2 md=2 lg=2">
+                <div className="col">
+                {graphActive === "showGraph" && <GraphComponent data = {response} />}
+                </div>
+                
+                <div className="col col-lg-3">
+                {graphActive === "showGraph" && <TableComponent data = {response} />}
+                </div>
+
+            </div>
+        </div>
+
+);
+}
+
+export default Query;
+
