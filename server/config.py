23f3e1c87dd510895ed9471f9f0dcb004923c42e
GraceBowen15 <71809742+GraceBowen15@users.noreply.github.com>
2022-10-12 11:34:51 +0800

    Delete server directory

--- server/config.py
+++
@@ -1,8 +0,0 @@
-# Neo4j database configuration
-NEO4j_URI = "bolt://localhost:7687"
-NEO4j_USER = "neo4j"
-NEO4j_PASSWORD = "test"
-
-# Jolt API configuration
-JOLT_URL = "http://localhost:7474/db/data/transaction/commit"
-
