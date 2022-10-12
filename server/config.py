573ab394ca59ce95ba5e0769ad46786d3d418edc
Yupeng Li <lypa520@gmail.com>
2022-10-12 16:28:07 +0800

    updated config file for SQLite database

--- server/config.py
+++ server/config.py
@@ -1,3 +1,5 @@
+import os
+
 # Neo4j database configuration
 NEO4j_URI = "bolt://localhost:7687"
 NEO4j_USER = "neo4j"
@@ -6,3 +8,12 @@ NEO4j_PASSWORD = "test"
 # Jolt API configuration
 JOLT_URL = "http://localhost:7474/db/data/transaction/commit"
 
+# set secret key
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'sshh!'
+
+# get absolute path
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# create database
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'sqlite.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
