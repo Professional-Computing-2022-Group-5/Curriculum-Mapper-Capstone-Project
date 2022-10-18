5ffe7459f7802f3c37ccde451b8e5ca506832f8e
GraceBowen15 <gracebbowen@gmail.com>
2022-10-18 15:11:52 +0800

    Updated UI

--- server/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_cors import CORS
-
-# create the application object
-app = Flask(__name__)
-# enable CORS
-CORS(app,supports_credentials=True)
-
-from app import routes, neo4jDB, api
